32a001c8064b45ad607dcb7e2a9580cb3a76b332
Mário Neto <82217027+Marianost0@users.noreply.github.com>
2022-04-23 00:14:48 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<meta name="author" content="">
+	<link rel="icon" href="/favicon.ico">
+	<link rel="apple-touch-icon" href="/favicon.ico">
+	<title>Seja Bem-vindo ao Customize!</title>
+</head>
+<body>
+	<section class="apresentacao">
+		<h1>Hello World!</h1>
+		<h2><em class="fa fa-user"></em></h2>
+	</section>
+<?php
